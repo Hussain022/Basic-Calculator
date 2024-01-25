bf9c06f9d1855d932a9404155a57cef22ccb6caa
Hussain022 <hussainahmed.vu@gmail.com>
2024-01-25 13:23:40 +0600

    one

---
+++ app.js
@@ -0,0 +1,103 @@
+let ans = 0
+let v = 0
+let t = ""
+let s,sb,m,d,e,n = false
+
+function sum(){
+    if(e==true){
+        if(n==true){
+            n = false
+            v = ans
+        }
+        e = false
+        document.getElementById("digit").innerText = v
+    }else{
+        v = ans
+        document.getElementById("digit").innerText = v
+        ans = 0
+    }
+    t = ""
+    s = true
+}
+
+function sub(){
+    if(e==true){
+        if(n==true){
+            n = false
+            v = ans
+        }
+        e = false
+        document.getElementById("digit").innerText = v
+    }else{
+        v = ans
+        document.getElementById("digit").innerText = v
+        ans = 0
+    }
+    t = ""
+    sb = true
+}
+
+function mul(){
+    if(e==true){
+        if(n==true){
+            n = false
+            v = ans
+        }
+        e = false
+        document.getElementById("digit").innerText = v
+    }else{
+        v = ans
+        document.getElementById("digit").innerText = v
+        ans = 0
+    }
+    t = ""
+    m = true
+}
+
+function div(){
+    if(e==true){
+        if(n==true){
+            n = false
+            v = ans
+        }
+        e = false
+        document.getElementById("digit").innerText = v
+    }else{
+        v = ans
+        document.getElementById("digit").innerText = v
+        ans = 0
+    }
+    t = "" 
+    d = true
+}
+
+function equal(){
+    if(s==true){
+        v += ans
+        s = false
+    }else if(sb==true){
+        v -= ans
+        sb = false
+    }else if(m==true){
+        v *= ans
+        m = false
+    }else if(d==true){
+        v /= ans
+        d = false
+    }
+    t = ""
+    document.getElementById("digit").innerText = v
+    ans = 0
+    e = true
+    n = false
+}
+
+function digit(x){
+    n = true
+    t += x
+    ans = +t
+    document.getElementById("digit").innerText = t
+}
+
+
+
